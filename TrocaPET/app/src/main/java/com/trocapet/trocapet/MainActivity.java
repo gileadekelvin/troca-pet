e07d574c4602f221b4d66d596529b1e7b6e19acb
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2017-07-28 22:05:46 -0300

    Configuração inicial do projeto

---
+++ TrocaPET/app/src/main/java/com/trocapet/trocapet/MainActivity.java
@@ -0,0 +1,45 @@
+package com.trocapet.trocapet;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_mapa:
+                    mTextMessage.setText(R.string.title_mapa);
+                    return true;
+                case R.id.navigation_perfil:
+                    mTextMessage.setText(R.string.title_perfil);
+                    return true;
+                case R.id.navigation_qrcode:
+                    mTextMessage.setText(R.string.title_qrcode);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
