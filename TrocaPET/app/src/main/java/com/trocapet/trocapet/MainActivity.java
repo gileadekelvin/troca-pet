3bea9c37325113eb0456cf7949b5673f392fdbee
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2017-07-29 13:14:47 -0300

    Esqueleto das Activities e Fragments Adição de todos os templates das activities. Adição do adapter e do fragments.
    Please enter the commit message for your changes. Lines starting

--- TrocaPET/app/src/main/java/com/trocapet/trocapet/MainActivity.java
+++ TrocaPET/app/src/main/java/com/trocapet/trocapet/MainActivity.java
@@ -1,46 +1,88 @@
 package com.trocapet.trocapet;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
-    private TextView mTextMessage;
+    private SectionsPageAdapter mSectionsPageAdapter;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private ViewPager mViewPager;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_perfil:
-                    mTextMessage.setText(R.string.title_perfil);
-                    return true;
-                case R.id.navigation_mapa:
-                    Intent intent = new Intent(getApplication(), MapsActivity.class);
-                    startActivity(intent);
-                case R.id.navigation_qrcode:
-                    mTextMessage.setText(R.string.title_qrcode);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_assignment);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_autorenew);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_attach_file);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_arrow:
+
+                        break;
+
+                    case R.id.navigation_perfil:
+                        Intent intent1 = new Intent(MainActivity.this, ActivityOne.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.navigation_mapa:
+                        Intent intent2 = new Intent(MainActivity.this, MapsActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.navigation_qrcode:
+                        Intent intent3 = new Intent(MainActivity.this, ActivityThree.class);
+                        startActivity(intent3);
+                        break;
+
+
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment());
+        adapter.addFragment(new Tab2Fragment());
+        adapter.addFragment(new Tab3Fragment());
+        viewPager.setAdapter(adapter);
     }
 
 }
