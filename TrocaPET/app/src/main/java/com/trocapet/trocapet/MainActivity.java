5ff6e282e046b8c7148cfcc801197201161a453d
lucaspk <jobson.silva@ccc.ufcg.edu.br>
2017-08-17 03:54:10 -0300

    iniciando brindes com nome e uma foto default

--- TrocaPET/app/src/main/java/com/trocapet/trocapet/MainActivity.java
+++ TrocaPET/app/src/main/java/com/trocapet/trocapet/MainActivity.java
@@ -1,40 +1,40 @@
 package com.trocapet.trocapet;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
     private static final String TAG = "MainActivity";
 
     private SectionsPageAdapter mSectionsPageAdapter;
 
     private ViewPager mViewPager;
 
+    String[] itemname ={
+            "Impressora Preto e Branco",
+            "Celular Moto G1",
+            "Headphone Samsung",
+            "Monitor 21' AOC",
+            "Coca-Cola 2L",
+            "Pen-drive Kingston 16GB",
+            "Mouse Sem Fio Multilaser",
+            "HD Externo 500 GB Seagate"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        setupViewPager(mViewPager);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_assignment);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_autorenew);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_attach_file);
+        this.setListAdapter(new ArrayAdapter<String>(
+                this, R.layout.my_list,
+                R.id.Itemname,itemname));
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
@@ -65,10 +65,7 @@ public class MainActivity extends AppCompatActivity {
                         intent3.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                         startActivity(intent3);
                         break;
-
-
                 }
-
 
                 return false;
             }
@@ -76,12 +73,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Tab1Fragment());
-        adapter.addFragment(new Tab2Fragment());
-        adapter.addFragment(new Tab3Fragment());
-        viewPager.setAdapter(adapter);
-    }
 
 }
